af9df430c522a0167a35046402369e65940a9a0b
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-04-12 08:23:14 -0700

    Server tests

---
+++ api/server.test.js
@@ -0,0 +1,22 @@
+const request = require("supertest");
+const server = require("./server.js");
+
+describe("server.js", () => {
+    describe("GET /", () => {
+      it("should respond with 200 OK", () => {
+        return request(server).get("/").expect(200);
+      });
+
+      it("should be JSON", () => {
+        return request(server).get("/").expect("Content-Type", /json/);
+      });
+
+      it("should get return {'It's alive!' }", () => {
+          return request(server)
+            .get("/").then(res => {
+                expect(res.body.api).toBe( "It's alive!" );
+            });
+        });
+  
+    });
+  });
---
+++ games/gamesModel.js
@@ -0,0 +1,35 @@
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+  insert,
+  update,
+  remove,
+  getAll,
+  findById,
+};
+
+async function insert(game) {
+  const [id] = await db('games').insert(game);
+
+  return db('games')
+    .where({id})
+    .first();
+}
+
+async function update(id, changes) {
+  return null;
+}
+
+function remove(id) {
+  return db('games')
+    .where( 'id', id )
+    .del();
+}
+
+function getAll() {
+  return db('games');
+}
+
+function findById(id) {
+  return null;
+}
